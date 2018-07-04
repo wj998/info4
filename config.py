9c3066e200c5ca28906faf1ee323d2cfebc44e27
wj <896656211@qq.com>
2018-07-04 21:18:48 +0800

    项目目录&集成数据库,迁移扩展&配置数据库的连接

--- config.py
+++ config.py
@@ -1,6 +1,10 @@
 class Config:
     DEBUG = True
 
+    # 配置数据库的链接和动态追踪修改
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:wangjian@localhost/info666'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 # 开发模式下的配置信息
 class developmentConfig(Config):
     DEBUG = True
--- manage.py
+++ manage.py
@@ -1,6 +1,10 @@
 from flask import Flask
 # 导入扩展flask-script扩展
 from flask_script import Manager
+# 导入扩展flask_migrate
+from flask_migrate import Migrate, MigrateCommand
+# 导入扩展flask_sqlalchemy
+from flask_sqlalchemy import SQLAlchemy
 
 # 导入配置文件中的字典
 from config import config
@@ -8,10 +12,15 @@ from config import config
 app = Flask(__name__)
 # 使用配置信息
 app.config.from_object(config['development'])
-
+# 实例化sqlalchemy对象
+db = SQLAlchemy(app)
 
 # 实例化管理对象
 manage = Manager(app)
+# 使用迁移框架
+Migrate(db, app)
+# 添加迁移命令
+manage.add_command('db', MigrateCommand)
 
 
 @app.route('/')
