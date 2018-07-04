8e3d95f95a779d43498bcbad85e334ff220b8f65
wj <896656211@qq.com>
2018-07-04 21:49:45 +0800

    项目目录&集成flask_session扩展实现状态保持&实例化redis数据库用来储存session信息

--- config.py
+++ config.py
@@ -1,9 +1,24 @@
+from redis import StrictRedis
+
+
 class Config:
     DEBUG = True
-
+    # 设置秘钥
+    SECRET_KEY = 'Uj1TCDDEnTbnkMeq7f6LhnMe1EkIejGI4Mvy0gQooRjOrehJ3m6TkQ=='
     # 配置数据库的链接和动态追踪修改
     SQLALCHEMY_DATABASE_URI = 'mysql://root:wangjian@localhost/info666'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # 配置状态保持存储的session信息
+    SESSION_TYPE = 'redis'
+    # 配置session的签名
+    SESSION_USE_SIGNER = True
+    # 构造redis的实例
+    SESSON_REDIS = StrictRedis(host='127.0.01', port=6379)
+    # 配置session的过期时间
+    PERMANENT_SESSION_LIFETIME = 86400
+
+
 
 # 开发模式下的配置信息
 class developmentConfig(Config):
--- manage.py
+++ manage.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, session
 # 导入扩展flask-script扩展
 from flask_script import Manager
 # 导入扩展flask_migrate
 from flask_migrate import Migrate, MigrateCommand
 # 导入扩展flask_sqlalchemy
 from flask_sqlalchemy import SQLAlchemy
+# 导入扩展 flask_session
+from flask_session import Session
+
+
+
 
 # 导入配置文件中的字典
 from config import config
@@ -12,6 +17,9 @@ from config import config
 app = Flask(__name__)
 # 使用配置信息
 app.config.from_object(config['development'])
+# 实例化Session对象
+Session(app)
+
 # 实例化sqlalchemy对象
 db = SQLAlchemy(app)
 
@@ -25,6 +33,7 @@ manage.add_command('db', MigrateCommand)
 
 @app.route('/')
 def index():
+    session['name'] = '2018'
     return 'index'
 
 if __name__ == '__main__':
