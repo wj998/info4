41fd22c6e1834f97726c1257acf8d29fd4c792f1
wj <896656211@qq.com>
2018-07-06 00:16:34 +0800

    项目目录&项目 migrations 创建   和 数据库迁移

--- manage.py
+++ manage.py
@@ -5,6 +5,8 @@ from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 #  导入create_app函数
 from info import create_app, db
+# 导入模型类,让模型类的文件被引用,用来迁移创建表
+from info import models
 
 app = create_app('development')
 
@@ -12,7 +14,7 @@ app = create_app('development')
 # 实例化管理对象
 manage = Manager(app)
 # 使用迁移框架
-Migrate(db, app)
+Migrate(app, db)
 # 添加迁移命令
 manage.add_command('db', MigrateCommand)
 
