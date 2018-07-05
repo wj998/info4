7121740393b616a76ef572069082f73f154cfcad
wj <896656211@qq.com>
2018-07-05 22:04:03 +0800

    项目目录&项目logo图片favicon.ico文件

--- info/modules/news/views.py
+++ info/modules/news/views.py
@@ -1,10 +1,18 @@
 # 导入蓝图函数
-from flask import session, render_template
+from flask import session, render_template, current_app
 
 from . import news_blue
 
 
+# 项目首页
 @news_blue.route('/')
 def index():
     session['name'] = '2018'
     return render_template('news/index.html')
+
+
+# 项目logo图片展示
+@news_blue.route('/favicon.ico')
+def favicon():
+    # 把项目logo文件发送给浏览器
+    return current_app.send_static_file('news/favicon.ico')
